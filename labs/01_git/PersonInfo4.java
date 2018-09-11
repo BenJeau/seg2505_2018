aa39ca020ec69f062c7dfde0b258d8ad82c8f36e
Aymen <amprodz15@gmail.com>
2018-09-11 18:23:03 -0400

    Add existing file INFO change

--- labs/01_git/PersonInfo4.java
+++ labs/01_git/PersonInfo4.java
@@ -1,9 +1,9 @@
 public class PersonInfo4 {
 
-    public static String firstName = "Alex";
-    public static String lastName = "Moujot";
-    public static int studentNo = 88499;
-    public static String description = "les ananas";
+    public static String firstName = "Aymen";
+    public static String lastName = "Mhamdii";
+    public static int studentNo = 300034572;
+    public static String description = "software engineering student";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
