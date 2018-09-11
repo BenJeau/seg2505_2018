af10d3253c68186bc6bb6ad7b46b0d37b315705c
Diedrick Ng <diedrickng@Diedricks-MacBook-Pro.local>
2018-09-11 17:45:58 -0400

    Modified branch Diedrick

--- labs/01_git/PersonInfo5.java
+++ labs/01_git/PersonInfo5.java
@@ -3,7 +3,7 @@ public class PersonInfo5 {
     public static String firstName = "Diedrick";
     public static String lastName = "Ngendahayo";
     public static int studentNo = 300008344;
-    public static String description = "Not the best lover";
+    public static String description = "the best programmer";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
