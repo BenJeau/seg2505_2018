de773da2714236770bedf60f3cbff001228d01e1
Diedrick Ng <dngen049@uottawa.ca>
2018-09-11 19:30:46 -0400

    Completed TheBest.java

--- labs/01_git/TheBest.java
+++ labs/01_git/TheBest.java
@@ -2,6 +2,11 @@
 public class TheBest {
     
     public static void main(String[] args) {
+         PersonInfo1.displayFr();
+         PersonInfo2.displayFr();
+         PersonInfo3.displayFr();
+         PersonInfo4.displayFr();
+         PersonInfo5.displayFr();
 
     }
 }
